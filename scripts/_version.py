6d79f6c9a2e1b68c8cbca25f9ebf8c961ba721c2
Jonas van den Berg <jonas@vandenbe.com>
2025-03-29 13:26:24 +0100

    Add version bump todo

--- scripts/_version.py
+++ scripts/_version.py
@@ -1,4 +1,6 @@
 # TODO remove win_winrt and mac_mediaremote from all player definitions
 # TODO remove fix_platform_identifiers below
 # TODO move "discord_application_id" into a general "discord" object
+# TODO add script that changes "latest" to "false" with all previous versions
+# in the live repository
 VERSION = 3
