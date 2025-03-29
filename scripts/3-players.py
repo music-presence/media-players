8c14b0afa0657882d63eacda8863df4772f9e5ca
Jonas van den Berg <jonas@vandenbe.com>
2025-03-29 13:20:40 +0100

    Add version bump todo

--- scripts/3-players.py
+++ scripts/3-players.py
@@ -9,8 +9,9 @@
 #
 #
 
-# TODO: remove win_winrt and mac_mediaremote from all player definitions
-# TODO: remove fix_platform_identifiers below
+# TODO remove win_winrt and mac_mediaremote from all player definitions
+# TODO remove fix_platform_identifiers below
+# TODO move "discord_application_id" into a general "discord" object
 VERSION = 3
 
 import os
